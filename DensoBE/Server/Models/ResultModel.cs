015c94dcff911351151e9540c273734b4030df8b
cur1en <73098281+pfffffffft@users.noreply.github.com>
2023-11-07 18:20:31 +0700

    init

---
+++ DensoBE/Server/Models/ResultModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Models
+{
+    public class ResultModel
+    {
+        public int[][][] MyProperty { get; set; }
+    }
+}
---
+++ DensoBE/Server/Models/WorkerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Models
+{
+    public class WorkerModel
+    {
+        public string WorkerId { get; set; }
+        public string WorkerName { get; set; }
+    }
+}
